fe5aae661816de3e4e37064c3a655acf9f5752d3
Paul Burt <paul.burt@gmail.com>
2015-01-11 13:53:54 +0000

    Updated to latest rum and updated core to use the new defc and mixin syntax.

--- src/tv/core.cljs
+++ src/tv/core.cljs
@@ -2,7 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.string :refer [blank? lower-case]]
             [cljs.core.async :refer [<!]]
-            [rum :include-macros true]
+            [rum :refer-macros [defc]]
             [tv.utils :refer [get-json! format-date get-weekday]]))
 
 (def state (atom {:schedule []}))
@@ -14,35 +14,32 @@
       (if (seq results)
         (swap! state assoc :schedule results)))))
 
-(rum/defc rum/static header [weekday]
-  [:header.jumbotron.text-center
+(defc header < rum/static [weekday]
+  [:header.jumbotron
    [:h1.animated.rubberBand
     [:span.fuchsia "sjónvarps"
      [:span.yellow "DOGEskrá "
       [:span.aqua weekday
        [:span.lime " (vá!)"]]]]]])
 
-(rum/defc rum/static tv-show
-  [{:keys [description originalTitle startTime title]}]
-  [:div.tv-show
-   [:h2 title
-    (if-not (or (blank? originalTitle)
-                (= (lower-case originalTitle) (lower-case title)))
-      [:small.fuchsia (str " (" originalTitle ")")])
-    [:p [:small.blue (format-date startTime "HH:mm")]]]
-   [:p description]])
+(defc tv-schedule < rum/static [schedule]
+  [:section#tv-schedule.container.animated.fadeIn
+   (for [{:keys [description originalTitle startTime title]} schedule]
+     [:div.tv-show
+      [:h2 title
+       (if-not (or (blank? originalTitle)
+                   (= (lower-case originalTitle) (lower-case title)))
+         [:small.fuchsia (str " (" originalTitle ")")])
+       [:p [:small.blue (format-date startTime "HH:mm")]]]
+      [:p description]])])
 
-(rum/defc rum/static tv-schedule [schedule]
-  [:section#tv-schedule.container.animated.fadeIn.text-center
-   (map tv-show schedule)])
-
-(rum/defc rum/reactive app []
+(defc app < rum/reactive []
   (let [{:keys [schedule]} (rum/react state)]
     (if (seq schedule)
-      [:div
+      [:div.text-center
        (header (get-weekday (:startTime (first schedule))))
        (tv-schedule schedule)])))
 
-(get-schedule!)
-
-(rum/mount (app) (.-body js/document))
+(defn ^:export mount [element]
+  (get-schedule!)
+  (rum/mount (app) element))
